76444c38609905f9f0a44a9e29b40024cd1d93d6
Carlos Del Real <carloshdelreal@gmail.com>
2021-12-29 16:48:50 -0500

    feat: questionary, createQuestionnaires and addQuestion

---
+++ src/models/questionary.ts
@@ -0,0 +1,72 @@
+import { guid } from '@/utils/uuid';
+import { Question } from '@/models/question';
+
+type Tag = {
+  id: string;
+  tag: string;
+};
+
+type TQuestionary = {
+  id: string;
+  questions: Array<Question['id']>;
+  subject: Array<Tag['id']>;
+};
+
+type TQuestionaryDB = Record<string, TQuestionary>;
+
+export class Questionary {
+  private static instance: Questionary;
+  questionnaires: TQuestionaryDB;
+
+  constructor() {
+    this.questionnaires = {
+      aaa: {
+        id: `aaa`,
+        questions: [`111`, `222`, `333`],
+        subject: [`trivia`],
+      },
+    };
+  }
+
+  public static getInstance(): Questionary {
+    if (!Questionary.instance) {
+      Questionary.instance = new Questionary();
+    }
+
+    return Questionary.instance;
+  }
+
+  getQuestionnaires = (): TQuestionary[] => {
+    return Object.values(this.questionnaires);
+  };
+
+  getQuestionary = (id: string): TQuestionary => {
+    return this.questionnaires[id];
+  };
+
+  createQuestionary = (
+    questions: string[],
+    subject: string[],
+  ): TQuestionary => {
+    const id = guid();
+    this.questionnaires[id] = {
+      id,
+      questions,
+      subject,
+    };
+
+    return this.questionnaires[id];
+  };
+
+  addQuestion = (
+    questionaryId: string,
+    questionId: string,
+  ): TQuestionary | null => {
+    const questionary = this.questionnaires[questionaryId];
+    if (questionary) {
+      questionary.questions.push(questionId);
+      return questionary;
+    }
+    return null;
+  };
+}
